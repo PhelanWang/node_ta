fc2520a14aefb45c19d7595e33c32eb15cddc71f
PhelanWang <1825633959@qq.com>
2018-10-09 21:23:02 +0800

    init commit

--- agent_disk_scan.py
+++ agent_disk_scan.py
@@ -111,7 +111,65 @@ def my_cross_memory(subtask_id, args):
                         result = 0,
                         brief = 'done',
                         detail = report)
-    
+
+
+@agent.entry("virtual_disk_scan", version="1.0.1")
+def my_vdisk_scan(subtask_id, args):
+    import os
+    import time
+
+    keyword = args['keyword']
+    data = ''
+    if keyword == '':
+        keyword = ''
+    # keyword=u'经济 15% 南洋科技'
+    m_list = keyword.split(' ')
+    # print m_list
+    s = ''
+    # 拼接关键字
+    for item in m_list:
+        s += item + ' '
+
+    if not os.path.exists("/home/qemu/"):
+        os.mkdir("/home/qemu/")
+        os.system("chown qemu /home/qemu")
+        os.system("chmod 777 /home/qmeu")
+
+    root = '/blk/virtio_blk'
+
+    current_path = os.getcwd()
+
+    error = os.system(current_path + root + '/run.sh ' + '/home/qemu/virtio_blk.log ' + s)
+
+    num = len(m_list)  # 关键字个数
+
+    if not error:
+        while num:
+            for filename in m_list:
+                filepath = '/tmp/virtio_blk/' + filename + '.txt'
+                if os.path.exists(filepath):
+                    num -= 1
+                    # 说明文件个数不够，继续等待知道文件个数和m_list长度大小一致
+            print num
+            if num:
+                num = len(m_list)
+                # 睡眠1秒在查找
+                time.sleep(1)
+
+                # 此时文件必定以及全部存在
+        for filename in m_list:
+            filepath = '/tmp/virtio_blk/' + filename + '.txt'
+            fo = open(filepath)
+            try:
+                data += fo.read() + '\n'
+            finally:
+                fo.close()
+        print 'data:', data.replace('[.:,;\\]', '')
+        return xmlrpclib.Binary(data.replace('[.:,;\\]', ''))
+    else:
+        print 'error'
+        return xmlrpclib.Binary('error')
+
 
 # Execute this while run this agent file directly
 if not is_load_external():
