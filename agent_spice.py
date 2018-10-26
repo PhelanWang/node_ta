1b67fe5f8f7f26e6d093623875766e9cc25bb352
PhelanWang <1825633959@qq.com>
2018-10-26 15:35:44 +0800

    modify spice

--- agent_spice.py
+++ agent_spice.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from time import sleep
 __author__ = 'Henry'
 # Determine whether loaded by agent_loader
@@ -19,10 +20,11 @@ def spice(subtask_id, args):
     # Post report to switch server:
     # agent.post_report(subtask_id, severity, result, brief, detail, json_data)
     import os, time, ConfigParser
+    from spice_vnc.format_data import get_format_data
     conf = ConfigParser.ConfigParser()
     conf.read('spice_vnc.conf')
     port = conf.get('SPICE', 'port', '5900')
-    cmd = 'tshark -i any -n -f \"src port '+port+'\" -d \"tcp.port=='+port+',vnc\" -a duration:120 -w data.cap'
+    cmd = 'tshark -i any -n -f "src port '+port+'" -d "tcp.port=='+port+',vnc" -a duration:120 -w data.cap'
     print 'please wait 2 mins,spice is testing...'
     print cmd
     os.system(cmd)
@@ -36,16 +38,18 @@ def spice(subtask_id, args):
     file = open('spice.txt')
     result = file.read()
     file.close()
+    result = get_format_data(result, 15)
     #print result
     os.system('rm -rf data.cap')
     # json_data is default as None
-    print result
+    for line in result:
+        print line
     agent.post_report(subtask_id,
                       severity=0,
                       result=1,
                       brief='SPICE',
-                      detail=u'spice',
-                      json_data={'result': str(result)[100:15000]})
+                      detail='获取SPICE协议的数据内容，若有明文，则可能部分数据未加密!\n',
+                      json_data=result)
 
 # Execute this while run this agent file directly
 if not is_load_external():
