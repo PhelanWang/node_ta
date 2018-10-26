b858d0262bb6a5908cc975d0a4798d4cb6f5c17c
PhelanWang <1825633959@qq.com>
2018-10-26 13:58:23 +0800

    modify mmount

--- agent_mmount.py
+++ agent_mmount.py
@@ -20,6 +20,7 @@ if not is_load_external():
 
 # OK
 # 对于同一个磁盘mount两个会出错，尝试只启动一台
+# 格式修改完成
 @agent.entry("mmount", version="1.0.2")
 def mmount_test(subtask_id, args):
     from virus_scan.mount import Mounter
@@ -36,37 +37,40 @@ def mmount_test(subtask_id, args):
         
     mt = Mounter(disk_path, disk_name)
     # print 'start mounting...'
-    mount = mt.mount()
+    # mount = mt.mount()
     report = mt.getReport()
     rpt=''
     detail=''
     brief=''
+    print '---------------'
     print report
-    
-    if(report==False):
-            rpt='启动虚拟机失败，该磁盘不可引导或未初始化'
-            brief='启动虚拟机失败'
-            detail='该磁盘不可引导或未初始化'
+    print '---------------'
+
+    if report == False:
+        rpt = '启动虚拟机失败，该磁盘不可引导或未初始化。\n'
+        brief = '启动虚拟机失败。\n'
+        detail = '该磁盘不可引导或未初始化。\n'
     else:
-        brief ='成功启动虚拟机'
-        detail=report
-        rpt=report
-    print 'report:', rpt
+        brief = '成功启动虚拟机'
+        detail = '对于同一个磁盘，尝试对该磁盘启动两个虚拟机。\n'
+        rpt = report
+
+    print '----'
+    print detail
+    print '----'
     agent.post_report(subtask_id,
                           severity=1,
                           result=0,
                           brief=brief,
                           detail=detail,
-                          json_data={'mm_result': rpt})
-
-
+                          json_data=report)
 
 
 # Execute this while run this agent file directly
 if not is_load_external():
     # Run agent
     args = {}
-    args["path"] = '/root/data/c17495b5-e94c-4df7-aeee-ac1c788df145/images/627b8e93-cf19-486c-aa4b-91000818b459/26bc6f9f-4b4f-4123-9442-35d624deeb5d'
-    args["name"]  = '26bc6f9f-4b4f-4123-9442-35d624deeb5d'
+    args["path"] = '/root/PycharmProjects/96d9b1b5-2f45-4baf-8462-5a166c87a3bb'
+    args["name"] = '96d9b1b5-2f45-4baf-8462-5a166c87a3bb'
     mmount_test(0, args)
     # agent.run()
